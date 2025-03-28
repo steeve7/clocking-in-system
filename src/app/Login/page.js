7289e049a56c5ebeb316720f7c300c6c66b12243
ofoegbu stephen <stephenchidindu12@gmail.com>
2025-03-28 14:09:05 +0100

    in progress

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -6,7 +6,7 @@ import { doc, getDoc, updateDoc, arrayUnion, setDoc } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 import { FaSignInAlt } from "react-icons/fa";
 
-export default function page() {
+export default function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
