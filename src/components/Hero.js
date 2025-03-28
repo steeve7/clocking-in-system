e7bcf2b3d6703089d2a62e77df0811818830da98
ofoegbu stephen <stephenchidindu12@gmail.com>
2025-03-28 14:46:13 +0100

    in progress

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
         <div className="flex flex-row justify-center items-center gap-4">
           <div className="flex flex-row items-center gap-2 border bg-white border-white text-blue-900 rounded-xl px-2 py-2 hover:transition ease-in-out">
             <FaUser />
-            <Link href={"/login"} className="font-roboto font-bold text-[20px]">
+            <Link href="/login" className="font-roboto font-bold text-[20px]">
               Sign-In
             </Link>
           </div>
