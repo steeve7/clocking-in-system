15c283810726e9654f520f12556330e98f65f1e6
ofoegbu stephen <stephenchidindu12@gmail.com>
2025-04-12 11:22:59 +0100

    login done

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,143 +1,154 @@
-'use client';
-import React, { useState, useEffect } from "react";
-import { auth, db } from "@/lib/firebase";; 
+"use client";
+import React, { useEffect, useState } from "react";
+import { auth, db } from "@/lib/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { doc, getDoc, updateDoc, arrayUnion, setDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc, arrayUnion } from "firebase/firestore";
 import { useRouter } from "next/navigation";
-import { FaSignInAlt } from "react-icons/fa";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { FaSignInAlt, FaEye, FaEyeSlash } from "react-icons/fa";
 
 export default function Login() {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const [success, setSuccess] = useState("")
-  const router = useRouter();
+  const [success, setSuccess] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  // const [localIP, setLocalIP] = useState("");
+  const router = useRouter();
 
-const today = new Date().toISOString().split("T")[0];
+  // useEffect(() => {
+  //   const getLocalIP = async () => {
+  //     const pc = new RTCPeerConnection({ iceServers: [] });
+  //     pc.createDataChannel("");
+  //     pc.onicecandidate = (event) => {
+  //       if (event.candidate) {
+  //         const ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3})/;
+  //         const ipMatch = event.candidate.candidate.match(ipRegex);
+  //         if (ipMatch) {
+  //           setLocalIP(ipMatch[1]);
+  //           pc.close();
+  //         }
+  //       }
+  //     };
+  //     await pc.createOffer().then((offer) => pc.setLocalDescription(offer));
+  //   };
 
+  //   getLocalIP();
+  // }, []);
 
-async function handleLogin(e) {
-  e.preventDefault();
-  setError("");
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+    setSuccess("");
 
-  try {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    const user = userCredential.user;
+    // const allowedIPs = ["172.20.10.1"]; // Replace with your allowed local IPs
 
-    // Fetch user role
-    const userDocRef = doc(db, "users", user.uid);
-    const userDoc = await getDoc(userDocRef);
+    // if (!allowedIPs.includes(localIP)) {
+    //   setError("Access Denied! Connect to the required network.");
+    //   return;
+    // }
 
-    if (userDoc.exists()) {
-      const userData = userDoc.data();
-      const role = userData.role?.toLowerCase(); // Normalize to lowercase
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
 
-      // Auto-mark attendance
-      await updateDoc(userDocRef, {
-        attendance: arrayUnion({
-          date: today,
-          status: "Active",
-          timestamp: new Date(),
-        }),
-      });
+      const userDocRef = doc(db, "users", user.uid);
+      const userDoc = await getDoc(userDocRef);
 
-      // Redirect based on role
-      if (role === "admin") {
-        setSuccess("Login successfully");
-        router.push("/admin");
-      } else if (role === "employee" || role === "manager") {
-        setSuccess("Login successfully");
-        router.push("/faceDetection");
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        const role = userData.role?.toLowerCase();
+
+        await updateDoc(userDocRef, {
+          attendance: arrayUnion({
+            date: new Date().toISOString().split("T")[0],
+            status: "Active",
+            timestamp: new Date(),
+          }),
+        });
+
+        setSuccess("Login successful");
+        setLoading(false);
+        if (role === "admin") {
+          router.push("/admin");
+        } else if (role === "employee" || role === "manager") {
+          setLoading(false);
+
+          router.push("/faceDetection");
+        } else {
+          setError("Unauthorized role.");
+        }
       } else {
-        setError("Unauthorized role.");
+        setError("User data not found.");
       }
-    } else {
-      setError("User data not found.");
+    } catch (error) {
+      console.error("Login Error:", error);
+      setError("Invalid email or password.");
     }
-  } catch (error) {
-    console.error("Login Error:", error);
-    setError("Invalid email or password.");
-  }
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex justify-center">
+      <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12 w-full">
+          <div className="flex justify-center items-center">
+            <h1 className="font-Euclid font-bold text-[30px] text-blue-500">SmartFace Check-In</h1>
+          </div>
+          <div className="mt-12 flex flex-col items-center">
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            {success && <p className="text-orange-500">{success}</p>}
+            <div className="w-full flex-1 mt-8">
+              <form className="mx-auto max-w-xs" onSubmit={handleLogin}>
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <div className="relative">
+                  <input
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <div
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="cursor-pointer absolute inset-y-0 right-0 flex items-center mt-5 justify-center px-8 text-gray-600"
+                  >
+                    {showPassword ? <FaEye /> : <FaEyeSlash />}
+                  </div>
+                </div>
+
+                <button
+                  className={`mt-5 tracking-wide gap-2 cursor-pointer font-semibold text-white w-full py-4 rounded-lg transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none ${
+                    loading
+                      ? "bg-gray-400 cursor-not-allowed"
+                      : "bg-blue-500 hover:bg-blue-600"
+                  }`}
+                  disabled={loading}
+                >
+                  <FaSignInAlt color="white" />
+                  {loading ? "Signing In..." : "Sign In"}
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 bg-green-100 text-center hidden md:flex">
+          <div
+            className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+            style={{ backgroundImage: "url('/images/background.svg')" }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
 }
-   return (
-     <div>
-       <div className="contain py-16">
-         <div className="max-w-lg mx-auto shadow px-6 py-7 rounded overflow-hidden">
-           {error && <p style={{ color: "red" }}>{error}</p>}
-           {success && <p className="text-orange-500">{success}</p>}
-           <h2 className="text-2xl uppercase font-medium mb-1 font-Euclid">
-             Login
-           </h2>
-           <p className="text-gray-600 mb-6 text-sm font-normal font-Poppins">
-             Welcome! So good to have you back!
-           </p>
-           <form autoComplete="off" onSubmit={handleLogin}>
-             <p className="text-red-500"></p>
-             <div className="space-y-2">
-               <div>
-                 <label
-                   htmlFor="email"
-                   className="text-gray-600 mb-2 block font-Euclid font-medium"
-                 >
-                   Email address
-                 </label>
-                 <input
-                   type="email"
-                   name="email"
-                   id="email"
-                   className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-teal-500 placeholder-gray-400 placeholder:font-normal placeholder:font-Poppins"
-                   placeholder="Email"
-                   value={email}
-                   onChange={(e) => setEmail(e.target.value)}
-                   required
-                 />
-               </div>
-             </div>
-             <div className="space-y-2 mt-5">
-               <div>
-                 <label
-                   htmlFor="password"
-                   className="text-gray-600 mb-2 block font-medium font-Euclid"
-                 >
-                   Password
-                 </label>
-                 <div className="relative">
-                   <input
-                     type={`${showPassword ? "text" : "password"}`}
-                     name="password"
-                     id="password"
-                     className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-teal-500 placeholder-gray-400 placeholder:font-normal placeholder:font-Poppins"
-                     placeholder="Password"
-                     value={password}
-                     onChange={(e) => setPassword(e.target.value)}
-                     required
-                   />
-                   <div onClick={() => setShowPassword(!showPassword)} className="cursor-pointer absolute inset-y-0 right-0 flex items-center px-8 text-gray-600 border-l border-gray-300">
-                   {showPassword ? 
-                    <FaEye />
-                    :
-                    <FaEyeSlash /> 
-                   }   
-                   </div>
-                 </div>
-               </div>
-             </div>
-             <div className="mt-5 flex flex-row justify-center items-center gap-2 py-2 px-2 w-full rounded-lg bg-gray-500">
-               <FaSignInAlt color="white" />
-               <button className="text-white font-normal font-Poppins">
-                 Login
-               </button>
-             </div>
-           </form>
-         </div>
-       </div>
-     </div>
-   );
-};
